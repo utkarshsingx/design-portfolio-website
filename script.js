006c63b3b61ba081d49af53e789e79e24ec2a6d2
Utkarsh <hauntedutkarsh@gmail.com>
2023-06-22 04:04:56 +0530

    updated code

---
+++ script.js
@@ -0,0 +1,25 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#wrapper"),
+  smooth: true,
+});
+
+var tl = gsap.timeline();
+
+tl.to("#page1", {
+  y: "100vh",
+  duration: 0,
+  scale: 0.6,
+});
+
+tl.to("#page1", {
+  y: "20vh",
+  duration: 1,
+  delay: 1,
+});
+
+tl.to("#page1", {
+  y: "0vh",
+  rotate: -360,
+  scale: 1,
+  duration: 0.8,
+});
